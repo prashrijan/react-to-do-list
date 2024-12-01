f50fd4aaeabb302d19c0782d6c0c2ae59f9222fe
Prashrijan <shresthaprashrijan@gmail.com>
2024-12-01 23:30:44 +1100

    react todo list

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <h2 className="text-2xl font-bold">React To Do List</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
